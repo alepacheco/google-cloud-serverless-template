50ccaa693d8fe0a6c28652776ad1e6fc70f4e42a
Alejandro Pacheco <alejandro.pacheco@CloudMargins-MacBook-Pro-3.local>
2019-12-09 17:03:46 +0000

    Adding local dev and build ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import {NAME} from './utils';
+
 export const myFunction = (request, response) => {
-    response.status(200).send('Hello World from TS!');
+    response.status(200).send(`Hello World from TS!. ${NAME}`);
 };
 
 export const event = (event, callback) => {
